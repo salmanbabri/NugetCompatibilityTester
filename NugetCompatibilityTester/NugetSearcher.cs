da1c8caf78d899288f4d788b0d987eb1e3dd41b2
Salman Babri <salmanbabri@gmail.com>
2021-03-05 03:37:24 +0500

    Working till extracting dependencies in raw form

---
+++ NugetCompatibilityTester/NugetSearcher.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace NugetCompatibilityTester
+{
+	public class NugetSearcher
+	{
+		private readonly IHttpClientFactory _factory;
+
+		public NugetSearcher(IHttpClientFactory factory)
+		{
+			_factory = factory;
+		}
+
+		public async Task Search(string packageId, string version)
+		{
+			string url = "https://azuresearch-usnc.nuget.org/query?q=PackageId:Newtonsoft.Json&semVerLevel=2.0.0";
+			var request = new HttpRequestMessage(HttpMethod.Get, url);
+
+			var client = _factory.CreateClient();
+			var response = await client.SendAsync(request);
+			var content = await response.Content.ReadAsStringAsync();
+
+			var foo = JObject.Parse(content);
+
+			var versionInformation = foo["data"]![0]!["versions"]!.ToList();
+
+			var nugetVersions = versionInformation.Select(v => new NugetVersion
+			{
+				Version = v["version"]!.ToString(),
+				DetailUrl = v["@id"]!.ToString()
+			}).ToList();
+
+			var versionInUse = nugetVersions.First(v => v.Version == version);
+
+			await ProcessVersionInUse(versionInUse);
+
+			Console.WriteLine("Done");
+		}
+
+		private async Task ProcessVersionInUse(NugetVersion versionInUse)
+		{
+			var client = _factory.CreateClient("decompress_gzip");
+			var request = new HttpRequestMessage(HttpMethod.Get, versionInUse.DetailUrl);
+			var response = await client.SendAsync(request);
+
+			var content = await response.Content.ReadAsStringAsync();
+
+			var bar = JObject.Parse(content);
+
+			string catalogUrl = bar["catalogEntry"]!.ToString();
+
+			await ProcessCatalog(catalogUrl);
+
+			Console.WriteLine("Done 2");
+		}
+
+		private async Task ProcessCatalog(string catalogUrl)
+		{
+			var client = _factory.CreateClient("decompress_gzip");
+			var request = new HttpRequestMessage(HttpMethod.Get, catalogUrl);
+			var response = await client.SendAsync(request);
+
+			var content = await response.Content.ReadAsStringAsync();
+
+			var zoo = JObject.Parse(content);
+
+			var dependencies = zoo["dependencyGroups"]?.ToList() ?? new List<JToken>();
+
+			//Todo: Extract .NET frameworks info from dependencies.
+
+			Console.WriteLine("Done 3");
+		}
+	}
+
+	public class NugetVersion
+	{
+		public string Version { get; set; } = "";
+		public string DetailUrl { get; set; } = "";
+	}
+}
---
+++ NugetCompatibilityTester/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace NugetCompatibilityTester
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var host = GetHostBuilder().Build();
+
+			using var serviceScope = host.Services.CreateScope();
+			var services = serviceScope.ServiceProvider;
+
+			var myService = services.GetRequiredService<NugetSearcher>();
+			await myService.Search("Newtonsoft.Json", "12.0.3");
+		}
+
+		private static IHostBuilder GetHostBuilder()
+		{
+			return new HostBuilder()
+			       .ConfigureServices((_, services) =>
+			       {
+				       services.AddHttpClient();
+				       services.AddHttpClient("decompress_gzip").ConfigurePrimaryHttpMessageHandler(messageHandler =>
+				       {
+					       var handler = new HttpClientHandler();
+
+					       if (handler.SupportsAutomaticDecompression)
+						       handler.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+					       return handler;
+				       });
+				       services.AddTransient<NugetSearcher>();
+			       })
+			       .UseConsoleLifetime();
+		}
+	}
+}
+
+/*
+	 services.AddHttpClient<XApiClient>().ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+	{
+		AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+	}).AddPolicyHandler(request => timeout);
+*/
